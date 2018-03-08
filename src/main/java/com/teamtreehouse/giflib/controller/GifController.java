66fdfc833a136dd650fcb3b1ee75164e3c8c1683
Saif Tase <saif.tase@sulzer-us.com>
2018-03-08 18:18:16 -0500

    Pass a GIF data object into gif-details template, and render when user visits /gif.

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,8 +1,12 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
 
 @Controller
 public class GifController {
@@ -13,8 +17,9 @@ public class GifController {
     }
 
     @RequestMapping("/gif")
-    @ResponseBody
-    public String randomGif() {
-        return "A random GIF.";
+    public String gifDetails(ModelMap modelMap) {
+        Gif gif = new Gif("compiler-bot", LocalDate.of(2015,2,13), "Saif Tase", true);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
