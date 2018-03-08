37a9c73a77b9cb8e08ea86d2a19da94ab8bd20e4
Saif Tase <saif.tase@sulzer-us.com>
2018-03-08 16:49:46 -0500

    Added homepage and /gif route

---
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -0,0 +1,21 @@
+package com.teamtreehouse.giflib.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GifController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String listGifs() {
+        return "List of all the GIFs!";
+    }
+
+    @RequestMapping("/gif")
+    @ResponseBody
+    public String randomGif() {
+        return "A random GIF.";
+    }
+}
